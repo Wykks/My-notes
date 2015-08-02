34ab223ab80f801533528eeaf03648a32147652d
Wykks <wykks@users.noreply.github.com>
2015-08-02 15:28:44 +0200

    Use decorators

--- components/Application.js
+++ components/Application.js
@@ -6,6 +6,16 @@ import ApplicationStore from '../stores/ApplicationStore';
 import { connectToStores, provideContext } from 'fluxible-addons-react';
 import { handleHistory } from 'fluxible-router';
 
+@provideContext
+@handleHistory
+@connectToStores([ApplicationStore], (context, props) => {
+    const appStore = context.getStore(ApplicationStore);
+    return {
+        currentPageName: appStore.getCurrentPageName(),
+        pageTitle: appStore.getPageTitle(),
+        pages: appStore.getPages()
+    };
+})
 class Application extends React.Component {
     render() {
         var Handler = this.props.currentRoute.get('handler');
@@ -27,15 +37,4 @@ class Application extends React.Component {
     }
 }
 
-export default handleHistory(provideContext(connectToStores(
-    Application,
-    [ApplicationStore],
-    function (context, props) {
-        var appStore = context.getStore(ApplicationStore);
-        return {
-            currentPageName: appStore.getCurrentPageName(),
-            pageTitle: appStore.getPageTitle(),
-            pages: appStore.getPages()
-        };
-    }
-)));
+export default Application;
